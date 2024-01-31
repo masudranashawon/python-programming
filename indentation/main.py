2e107863db0ca13c7fb8e39a6e942835f982eeff
masudranashawon <mohammadmasudrana3396@gmail.com>
2024-01-31 18:34:24 +0600

    indentation concept

---
+++ indentation/main.py
@@ -0,0 +1,14 @@
+name = "Masud rana shawon"
+role = "mern stack developer"
+
+print(name)
+
+if (name != role):
+    print("Name and role are not the same value")
+
+print(2+2)
+
+a = 10
+
+age = 10
+new_age = 10
