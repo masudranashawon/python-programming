55928b6b93c796ac8fc79753167f8a9cb3fcc1ba
masudranashawon <mohammadmasudrana3396@gmail.com>
2024-01-31 18:32:52 +0600

    string concept

---
+++ string/main.py
@@ -0,0 +1,9 @@
+studentName = "masud rana shawon"
+student_role = "Web developer"
+_id = "sd325hj34h5"
+studentAge = 2023-1996
+StudentSubject = "accounting"
+
+print(studentName + ", age: " + str(studentAge))
+print(_id)
+print(StudentSubject)
